9a55e55900ab315f508c404f93f9c9c97076734a
Michael Bennett <michaelbennett.dev@gmail.com>
2018-09-27 21:02:29 -0700

    Finally got a somewhat-comprehensible system.

--- folderbot/folderbot.py
+++ folderbot/folderbot.py
@@ -4,24 +4,35 @@ from utils import utils as ut
 import socket
 from time import sleep
 
+class Config:
+    def __init__(self, channel, socket):
+        self.channel = channel
+        self.socket = socket
+
 def connect():
     s = socket.socket()
     s.connect((cf.HOST, cf.PORT))
     s.send("PASS {}\r\n".format(cf.PASS).encode("utf-8"))
     s.send("NICK {}\r\n".format(cf.NICK).encode("utf-8"))
-    s.send("JOIN {}\r\n".format(cf.CHANNEL).encode("utf-8"))
+    s.send("JOIN #{}\r\n".format(cf.CHANNEL).encode("utf-8"))
     return s
 
-s = connect()
-itr = 0
-while(True):
-    itr+=1
-    response = s.recv(1024).decode("utf-8")
-    if response == "PING :tmi.twitch.tv\r\n": # this is very temporary
-        s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
-    else:
-        print(response)
-    if itr == 10:
-        ut.send(s, "Hello, not sure why this isn't working...")
-        itr = 0
-    sleep(5)
+_config = Config(cf.CHANNEL, connect())
+_api = ut.API(_config)
+
+def main():
+    itr = 0
+    while(True):
+        itr+=1
+        print('Trying to get response...')
+        response = _api.resp()
+        print('Received response.')
+        if response is not None:
+            print(response)
+        if itr == 10:
+            _api.send("Hello, not sure why this isn't working...")
+            itr = 0
+        sleep(5)
+
+if __name__ == "__main__":
+    main()
--- folderbot/utils/utils.py
+++ folderbot/utils/utils.py
@@ -2,20 +2,40 @@
 # https://www.instructables.com/id/Twitchtv-Moderator-Bot/
 
 # Ideally, I'd like to use Twitch's ""actual"" API (i.e. no IRC, which is apparently being deprecated) but that can wait for the weekend
-class Config:
-    def __init__(self, channel, socket):
-        self.channel = channel
-        self.socket = socket
 
-__CONFIGURATION__ = Config(None, None)
+import select
 
-def send(message):
-    __CONFIGURATION__.socket.send("PRIVMSG #{} :{}".format(__CONFIGURATION__.channel, message).encode("utf-8"))
+def send(socket, channel, message):
+    socket.send("PRIVMSG #{} :{}".format(channel, message).encode("utf-8"))
 
-def ban(user):
-    # This is probably going to be unused for now
-    # Just so I don't forget the style
-    send(".ban {}".format(user))
 
-def timeout(socket, user, seconds=10):
-    send(".timeout {} {}".format(user, seconds))
+def get_resp_or_none(socket):
+    print("Going to find information from sockets.")
+    read_s, write_s, error_s = select.select([socket], [], [], 5)
+    print("Found socket information.")
+    for _s in read_s:
+        print("There is a socket ready to read.")
+        response = _s.recv(1024).decode("utf-8")
+        if response == "PING :tmi.twitch.tv\r\n": # this is very temporary
+            _s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+            return "PING"
+        else:
+            return response
+    return None
+
+
+class API:
+    def __init__(self, config):
+        self.socket = config.socket
+        self.channel = config.channel
+        self.pings = 0
+
+    def send(self, message):
+        send(self.socket, self.channel, message)
+
+    def resp(self):
+        response = get_resp_or_none(self.socket)
+        if response == "PING":
+            self.pings += 1
+            response = None
+        return response
