c302d02dbf761f83fbd4f8fab5643043214b48dd
Michael Bennett <michaelbennett.dev@gmail.com>
2018-10-06 14:59:37 -0700

    Added an example of a configuration file for the bot.

---
+++ folderbot/config/bot_configuration_example.py
@@ -0,0 +1,5 @@
+HOST = 'irc.chat.twitch.tv'
+PORT = 6667
+NICK = 'Your bot\'s nickname.'
+PASS = 'An OAuth token, should look like: oauth:[string of alphanumeric characters]'
+CHANNEL = 'The channel you want your bot to join.'
